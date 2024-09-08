449cda3f9b5d8279158f7ceca1c9dfea8a5d8236
Utk984 <utkarshagarwal984@gmail.com>
2024-09-09 00:19:09 +0530

    Eval script

--- src/eval.py
+++ src/eval.py
@@ -6,20 +6,25 @@ from classes.board import Board
 from classes.cell import Cell, CellType
 from classes.game import Direction, Game
 from classes.snake import Snake
+from input.bestfs import bestfs
 from input.bfs import bfs
 from input.dfs import dfs
 from utils.plots import plots
 
 
-def run_comparisons(board_size):
+def run_comparisons(board_size, bfs_eval=True):
     results = []
     for i in tqdm(board_size):
         game = create_random_game(i)
 
         dfs_result = dfs(game, eval=True)
-        bfs_result = bfs(game, eval=True)
+        if bfs_eval:
+            bfs_result = bfs(game, eval=True)
+        else:
+            bfs_result = None
+        best_result = bestfs(game, eval=True)
 
-        results.append((dfs_result, bfs_result))
+        results.append((dfs_result, bfs_result, best_result))
     return results
 
 
@@ -42,5 +47,5 @@ if __name__ == "__main__":
     results = run_comparisons([3] * 10)
     plots(results)
 
-    results = run_comparisons([2, 2, 2, 3, 3, 3, 4, 4, 4])
+    results = run_comparisons([2, 3, 4, 5, 6, 7, 8], bfs_eval=False)
     plots(results)
--- src/utils/plots.py
+++ src/utils/plots.py
@@ -3,16 +3,24 @@ import matplotlib.pyplot as plt
 
 def plots(results):
     dfs_results = [result[0] for result in results]
-    bfs_results = [result[1] for result in results]
-
     dfs_num_moves = [result["num_moves"] for result in dfs_results]
-    bfs_num_moves = [result["num_moves"] for result in bfs_results]
-
     dfs_max_open_size = [result["max_open_size"] for result in dfs_results]
-    bfs_max_open_size = [result["max_open_size"] for result in bfs_results]
-
-    bfs_time = [result["time"].total_seconds() for result in bfs_results]
     dfs_time = [result["time"].total_seconds() for result in dfs_results]
+
+    if results[0][1] is None:
+        bfs_num_moves = [0] * len(dfs_num_moves)
+        bfs_max_open_size = [0] * len(dfs_num_moves)
+        bfs_time = [0] * len(dfs_num_moves)
+    else:
+        bfs_results = [result[1] for result in results]
+        bfs_num_moves = [result["num_moves"] for result in bfs_results]
+        bfs_max_open_size = [result["max_open_size"] for result in bfs_results]
+        bfs_time = [result["time"].total_seconds() for result in bfs_results]
+
+    best_results = [result[2] for result in results]
+    best_num_moves = [result["num_moves"] for result in best_results]
+    best_max_open_size = [result["max_open_size"] for result in best_results]
+    best_time = [result["time"].total_seconds() for result in best_results]
 
     _, axs = plt.subplots(1, 3, figsize=(12, 6))
 
@@ -29,6 +37,13 @@ def plots(results):
         label="BFS - Number of Moves",
         color="g",
         marker="x",
+    )
+    axs[0].plot(
+        range(1, len(best_num_moves) + 1),
+        best_num_moves,
+        label="BestFS - Number of Moves",
+        color="y",
+        marker="s",
     )
     axs[0].set_title("Number of Moves Comparison")
     axs[0].set_xlabel("Game Instance")
@@ -50,6 +65,13 @@ def plots(results):
         color="orange",
         marker="x",
     )
+    axs[1].plot(
+        range(1, len(best_max_open_size) + 1),
+        best_max_open_size,
+        label="BestFS - Max Open Size",
+        color="green",
+        marker="s",
+    )
     axs[1].set_title("Max Open Size Comparison")
     axs[1].set_xlabel("Game Instance")
     axs[1].set_ylabel("Max Open Size")
@@ -70,6 +92,13 @@ def plots(results):
         color="brown",
         marker="x",
     )
+    axs[2].plot(
+        range(1, len(best_time) + 1),
+        best_time,
+        label="BestFS - Time",
+        color="black",
+        marker="s",
+    )
     axs[2].set_title("Time Comparison")
     axs[2].set_xlabel("Game Instance")
     axs[2].set_ylabel("Time (s)")
@@ -77,4 +106,5 @@ def plots(results):
     axs[2].grid(True)
 
     plt.tight_layout()
+    plt.title("Comparison of DFS, BFS and BestFS")
     plt.show()
